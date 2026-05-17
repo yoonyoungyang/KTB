ba269265d407e0e73b2674796e5d26e9087baa34
yang-yun-yeong <youngonly0118@gmail.com>
2026-05-17 13:21:06 +0900

    1차 목표 : 가수와 제목 배열 생성, 1번 문항인 구매 음악 골라달라는 멘트와 음악 목록 보여주기

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("LP 가게 시작!");
+        String[] singer = {"NewJeans", "IVE", "aespa", "IU", "BTS", "The Weeknd", "Olivia Rodrigo", "Harry Styles", 
+        "Miley Cyrus", "Sabrina Carpenter"};
+        String[] songName = {"Ditto", "LOVE DIVE", "Next Level", "Celebrity", "Dynamite", "Blinding Lights", "drivers license", "As It Was", "Flowers", "Espresso"};
+        System.out.println("구매할 음악을 골라주세요.");
+        for (int i=0; i<singers.length; i++) {
+            System.out.println((i+1) + ". " + singers[i] + " - " + songName[i]);
+        }
     }
 }
